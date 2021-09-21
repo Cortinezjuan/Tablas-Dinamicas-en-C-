b87a505b68bbe146a77890c8ba8edc150ec9ea6b
JuanJo Cortinez <cortinezjuan@gmail.com>
2021-09-21 10:22:11 -0300

    first commit

---
+++ CtrlProductos.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablasDinamicas
+{
+    class CtrlProductos : Conexion
+    {
+        public List<Object> consulta(String dato)
+        {
+            MySqlDataReader reader;
+            List<Object> lista = new List<object>();
+            string sql;
+            if(dato == null)
+            {
+                sql = "SELECT codigo, nombre, descripcion, precio_publico, existencias FROM datosproductos ORDER BY nombre ASC";
+
+            }
+            else
+            {
+                sql = "SELECT codigo, nombre, descripcion, precio_publico, existencias FROM datosproductos WHERE codigo LIKE'%"+dato+ "%' OR nombre LIKE'%" + dato + "%' OR precio_publico LIKE'%" + dato + "%' OR existencias LIKE'%" + dato + "%' ORDER BY nombre ASC";
+            }
+            try
+            {
+                MySqlConnection conexionBD = base.conexion();
+                conexionBD.Open();
+                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+                reader = comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    Productos _producto = new Productos();
+                    _producto.Codigo = int.Parse(reader.GetString(0));
+                    _producto.Nombre = reader.GetString("nombre");
+                    _producto.Descripcion = reader[2].ToString();
+                    _producto.Precio_publico = double.Parse(reader[3].ToString());
+                    _producto.Existencias = int.Parse(reader.GetString(4));
+                    lista.Add(_producto);
+
+                }
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            return lista;
+            
+        }
+    }
+}
---
+++ tienda.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.5.5-10.4.20-MariaDB : Database - tienda
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tienda` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `tienda`;
+
+/*Table structure for table `datosproductos` */
+
+DROP TABLE IF EXISTS `datosproductos`;
+
+CREATE TABLE `datosproductos` (
+  `codigo` varchar(20) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `precio_publico` decimal(10,0) DEFAULT NULL,
+  `existencias` int(10) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `datosproductos` */
+
+insert  into `datosproductos`(`codigo`,`nombre`,`descripcion`,`precio_publico`,`existencias`) values ('100','coca cola','coca cola 20l','1000',25),('101','sprite','sprite 1l','100',5),('102','fanta','fanta 2l','200',10),('103','paso de los toros','paso de los toros retornable','300',50);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
